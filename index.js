476077526391ffebdf0d2780d66c01961a526474
An Nuyen <annuyen@gmail.com>
2018-01-31 15:38:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+class BoardMember {
+  constructor(name, homeState, training) {
+    this.name = name
+    this.homeState = homeState
+    this.training = training
+  }
+
+  veto() {
+
+  }
+
+  approve() {
+
+  }
+
+  doCharity() {
+
+  }
+
+  releasePressStatement() {
+
+  }
+
+  sayHi(){
+
+  }
+}
